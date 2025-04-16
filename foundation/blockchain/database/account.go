c3120092c61441bfb3a24416ee39e88921fc38e7
obynonwane <biostechnologyng@gmail.com>
2025-04-16 17:56:50 +0100

    adding account struct

--- foundation/blockchain/database/account.go
+++ foundation/blockchain/database/account.go
@@ -2,6 +2,21 @@ package database
 
 import "errors"
 
+// Account represents information stored in the database for an individual account.
+type Account struct {
+	AccountID AccountID
+	Nonce     uint64
+	Balance   uint64
+}
+
+// newAccount constructs a new account value for use.
+func newAccount(accountID AccountID, balance uint64) Account {
+	return Account{
+		AccountID: accountID,
+		Balance:   balance,
+	}
+}
+
 // =============================================================================
 
 // AccountID represents an account id that is used to sign transactions and is
