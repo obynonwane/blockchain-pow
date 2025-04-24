a6fe2d6657cf1618aee8522621c8fb45e76707fe
obynonwane <biostechnologyng@gmail.com>
2025-04-24 01:53:28 +0100

    mempool 2

--- foundation/blockchain/mempool/mempool.go
+++ foundation/blockchain/mempool/mempool.go
@@ -2,6 +2,8 @@
 package mempool
 
 import (
+	"fmt"
+	"strings"
 	"sync"
 
 	"github.com/ardanlabs/blockchain/foundation/blockchain/database"
@@ -9,12 +11,60 @@ import (
 
 // Mempool represents a cache of transactions organised by account:nonce
 type Mempool struct {
-	mu       sync.RWMutex
-	pool     map[string]database.BlockTx
-	selectFn selector.Func
+	mu   sync.RWMutex
+	pool map[string]database.BlockTx
 }
 
 // New constructs a new mempool using the default sort strategy
 func New() (*Mempool, error) {
-	return NewWithStrategy(selector.StrategyTip)
+	return NewWithStrategy()
+}
+
+// NewWithStrategy constructs a new mempool with specified sort strategy
+func NewWithStrategy() (*Mempool, error) {
+	mp := Mempool{
+		pool: make(map[string]database.BlockTx),
+	}
+	return &mp, nil
+}
+
+// Count returns the current number of transactions in the pool.
+func (mp *Mempool) Count() int {
+	mp.mu.RLock()
+	defer mp.mu.RUnlock()
+
+	return len(mp.pool)
+}
+
+// Upsert adds or replace a transaction from th mempool
+func (mp *Mempool) Upsert(tx database.BlockTx) error {
+	mp.mu.Lock()
+	defer mp.mu.Unlock()
+
+	// CORE NOTE: Different blockchains have different algorithms to limit the
+	// size of the mempool. Some limit based on the amount of memory being
+	// consumed and some may limit based on the number of transaction. If a limit
+	// is met, then either the transaction that has the least return on investment
+	// or the oldest will be dropped from the pool to make room for new the transaction.
+
+	// For now my blockchain is not imposing any limits
+	key, err := mapKey(tx)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// ========================================================================================
+
+// mapKey is used to generate the map key.
+func mapKey(tx database.BlockTx) (string, error) {
+	return fmt.Sprintf("%s:%d", tx.FromID, tx.Nonce), nil
+}
+
+// accountFromMapKey extracts the account information from the mapKey
+func accountFromMapKey(key string) database.AccountID {
+	// splits key string and convert to account type returning th e0 indexed
+	return database.AccountID(strings.Split(key, ":")[0])
 }
